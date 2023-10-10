bc24014f30c43ac0285a1ad945ec48a3f8073a13
ragnamarie <ragna.steinboemer@gmail.com>
2023-10-10 18:14:17 +0200

    add button change color

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // please add questions here
   const questions = [
-    "Whose stage name is Sporty Spice?",
+    "What is the other name of Sporty Spice?",
     "How many members do the Spice Girls have?",
     "Robbie Williams is from which boy band?",
     "Which actor plays Roman in Succession?",
     "What is the capital of Denmark?",
+  ];
+
+  const answers = [
+    "Melanie C",
+    "5",
+    "Take That",
+    "Kieran Culkin",
+    "Copenhagen",
   ];
 
   function addQuestionBox(numberOfQuestions) {
@@ -23,11 +31,17 @@ document.addEventListener("DOMContentLoaded", function () {
       bookmark.setAttribute("src", "images/bookmark.png");
       bookmark.setAttribute("alt", "BOOKMARKS");
       questionBox.append(bookmark);
-      // trying to make button change
+      // makes bookmark change color
       bookmark.addEventListener("click", () => {
-        bookmark.setAttribute("src", "images/bookmark_filled.png");
-        bookmark.setAttribute("alt", "BOOKMARKS");
-        questionBox.append(bookmark);
+        if (bookmark.getAttribute("src") == "images/bookmark.png") {
+          bookmark.setAttribute("src", "images/bookmark_filled.png");
+          bookmark.setAttribute("alt", "BOOKMARKS");
+          questionBox.append(bookmark);
+        } else {
+          bookmark.setAttribute("src", "images/bookmark.png");
+          bookmark.setAttribute("alt", "BOOKMARKS");
+          questionBox.append(bookmark);
+        }
       });
       // creates question
       const question = document.createElement("p");
@@ -39,6 +53,12 @@ document.addEventListener("DOMContentLoaded", function () {
       answerButton.classList.add("question-box__answer-button");
       answerButton.textContent = "SHOW ANSWER";
       questionBox.append(answerButton);
+      //trying to show answer
+      answerButton.addEventListener("click", () => {
+        answerButton.classList.add("question-box__answer-button");
+        answerButton.textContent = answers[counter];
+        questionBox.append(answerButton);
+      });
       //creates category boxes container
       const boxContainer = document.createElement("div");
       boxContainer.classList.add("question-box__container");
