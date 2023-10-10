5e82f46016a693d7c18fccb1681bded8ece51209
ragnamarie <ragna.steinboemer@gmail.com>
2023-10-10 13:51:55 +0200

    added more functionality and button does turn black now yay

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ document.addEventListener("DOMContentLoaded", function () {
     '[data-js="question-section"]'
   );
 
+  // please add questions here
   const questions = [
     "Whose stage name is Sporty Spice?",
     "How many members do the Spice Girls have?",
     "Robbie Williams is from which boy band?",
+    "Which actor plays Roman in Succession?",
+    "What is the capital of Denmark?",
   ];
 
   function addQuestionBox(numberOfQuestions) {
@@ -20,10 +23,16 @@ document.addEventListener("DOMContentLoaded", function () {
       bookmark.setAttribute("src", "images/bookmark.png");
       bookmark.setAttribute("alt", "BOOKMARKS");
       questionBox.append(bookmark);
+      // need to add button functionality here?
+      bookmark.addEventListener("click", () => {
+        bookmark.setAttribute("src", "images/bookmark_filled.png");
+        bookmark.setAttribute("alt", "BOOKMARKS");
+        questionBox.append(bookmark);
+      });
       // creates question
       const question = document.createElement("p");
       question.classList.add("question-box__questions");
-      question.textContent = "What is 12 divided by 3?"; // this will be replaced by loop
+      question.textContent = questions[counter];
       questionBox.append(question);
       // creates button
       const answerButton = document.createElement("button");
@@ -34,12 +43,14 @@ document.addEventListener("DOMContentLoaded", function () {
       const boxContainer = document.createElement("div");
       boxContainer.classList.add("question-box__container");
       questionBox.append(boxContainer);
-      //creates category boxes -- needs loop within loop
-      const categories = document.createElement("p");
-      categories.classList.add("question-box__container__categories");
-      categories.textContent = "Math";
-      boxContainer.append(categories);
+      //creates category boxes -- need to add category array or something
+      for (i = 0; i < 3; i++) {
+        const categories = document.createElement("p");
+        categories.classList.add("question-box__container__categories");
+        categories.textContent = "Math";
+        boxContainer.append(categories);
+      }
     }
   }
-  addQuestionBox(3);
+  addQuestionBox(questions.length);
 });
