375a7cc49feba4bad1f15fc1317e25ce7499ab1d
ragnamarie <ragna.steinboemer@gmail.com>
2023-10-11 09:49:41 +0200

    add function to store q and a in array for now

--- js/form.js
+++ js/form.js
@@ -10,5 +10,29 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log(data);
     console.log(data.question);
     console.log(data.answer);
+
+    storeInput();
   });
+
+  function storeInput() {
+    // Initialize an empty array
+    const questionArray = [];
+    const answerArray = [];
+
+    // Get the input elements
+    const input1 = document.getElementById("question");
+    const input2 = document.getElementById("answer");
+
+    // Get the values of the input elements
+    const value1 = input1.value;
+    const value2 = input2.value;
+
+    // Add the values to the array
+    questionArray.push(value1);
+    answerArray.push(value2);
+
+    // Print the array to the console
+    console.log(questionArray);
+    console.log(answerArray);
+  }
 });
